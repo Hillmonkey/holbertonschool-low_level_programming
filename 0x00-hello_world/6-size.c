6ca3d48861ddbbcc79c45002b5abb247d37debd8
Larry Madeo <lmadeo76@gmail.com>
2017-02-09 05:16:36 +0000

    1st commit c program #5 and program #6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints out size in bytes of various variable types
+ * This can be run with a 32 bit compiler and a 64 bit compiler
+ * for extra insight!
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char myChar;
+	int myInt;
+	long int myLongInt;
+	long long int myLongLongInt;
+	float myFloat;
+
+	printf("Size of a char: %i byte(s)\n", sizeof(myChar));
+	printf("Size of an int: %i byte(s)\n", sizeof(myInt));
+	printf("Size of a long int: %i byte(s)\n", sizeof(myLongInt));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(myLongLongInt));
+	printf("Size of a float: %i bytes(s)\n", sizeof(myFloat));
+
+	return (0);
+}
