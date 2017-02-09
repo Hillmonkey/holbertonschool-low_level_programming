f38b31ee1a78c85a14c7ca0ec0e2e99f9d992949
Larry Madeo <lmadeo76@gmail.com>
2017-02-09 21:37:35 +0000

    first commit: 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * 0-positive_or_negative.c
+ *
+ * Description:
+ * This program prints
+ * a randomly generated number
+ * and outputs a string indicating
+ * if it is positive, negative, or zero
+ *
+ */
+
+/**
+  * main - this is the main function that runs everything
+  * @void: no arguments passed in
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n >= 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
+	return (0);
+}
