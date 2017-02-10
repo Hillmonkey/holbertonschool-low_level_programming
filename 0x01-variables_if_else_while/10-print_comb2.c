af8ed81d123812708294b8d584230e9a8624d33a
Larry Madeo <lmadeo76@gmail.com>
2017-02-10 06:56:22 +0000

    #10: first commit

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/*
+ * 10-print_comb2.c
+ *
+ * Description:
+ * This program prints
+ * all the base 10 numbers in order
+ * from 0 to 99
+ * No vars of type char ...
+ */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, offset;
+
+	offset = 48;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i + offset);
+			putchar(j + offset);
+			if (!((i == 9) && (j == 9)))
+				putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(10);
+	return (0);
+}
