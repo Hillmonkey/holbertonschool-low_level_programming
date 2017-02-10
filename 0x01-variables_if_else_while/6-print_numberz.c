aef400a71b265e4ec815c8225d2d92da516504f3
Larry Madeo <lmadeo76@gmail.com>
2017-02-10 03:03:32 +0000

    1st commit: 6-Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,30 @@
+#include <time.h>
+#include <stdio.h>
+/*
+ * 6-print_numberz.c
+ *
+ * Description:
+ * This program prints
+ * all the base 10 digits in order
+ * using putchar()
+ * No vars of type char ...
+ */
+
+/**
+  * main - this is the main function that runs everything
+  * @void: no arguments passed in
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int letter;
+	int i;
+
+	letter = '0';
+
+	for (i = 0; i < 10; i++)
+		putchar(letter + i);
+	putchar('\n');
+	return (0);
+}
