08d29ce8be25e70d65824346f9091eaff7cb589b
Larry Madeo <lmadeo76@gmail.com>
2017-02-15 03:41:44 +0000

    changed spelling of function name print_alphabet blah blah to include underscore in proper place.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- * print_alphabetx10 - prints alphabet times 10
+ * print_alphabet_x10 - prints alphabet times 10
  *
  * Return: Always return 0
  */
-void print_alphabetx10(void)
+void print_alphabet_x10(void)
 {
 		int i;
 		int j;
