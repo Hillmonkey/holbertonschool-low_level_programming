d68fb87eae7aadff0a85ded4e8231952fd66232d
Larry Madeo <lmadeo76@gmail.com>
2017-02-15 08:40:44 +0000

    added extra for loop, with mods for decrementing situation. Not very DRY at all!

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,18 +16,18 @@ void print_to_98(int n)
 		{
 			printf("%i", i);
 			if (i != 98)
-				printf(",");
+				printf(", ");
 			else
 				printf("\n");
 		}
 	}
 	else
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%i", i);
 			if (i != 98)
-				printf(",");
+				printf(", ");
 			else
 				printf("\n");
 		}
