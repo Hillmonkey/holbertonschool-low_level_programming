2da69936166032baa4af16609f351b456f609a29
Larry Madeo <lmadeo76@gmail.com>
2017-02-17 23:28:34 +0000

    2nd fizz buzz commit, now Betty is happy.

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,37 +2,29 @@
 
 /**
  * main - this is an implementation of the FizzBuzz(tm) algorithm
- * Return - 1 upon successful exit
+ * Return: 0 upon successful exit
  */
 int main(void)
 {
 	int i;
 
-	for(i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (!( i % 3 == 0 || i % 5 == 0))
-		{
-			printf ("%d", i);
-		}
+		if (!(i % 3 == 0 || i % 5 == 0))
+			printf("%d", i);
 		else
 		{
 			if ((i % 3 == 0) && (i % 5 != 0))
-			{
-				printf ("Fizz");
-			}
+				printf("Fizz");
 			else if ((i % 3 != 0) && (i % 5 == 0))
-				printf ("Buzz");
-			else  
-				printf ("FizzBuzz");
+				printf("Buzz");
+			else
+				printf("FizzBuzz");
 		}
 		if (i < 100)
-			printf (" ");
-		else	
-		{
-			printf ("\n");
-		}
-		
+			printf(" ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
-	
