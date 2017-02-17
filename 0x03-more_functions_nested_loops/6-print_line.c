cbe89cf07af29f74afe83234637050de195a05fb
Larry Madeo <lmadeo76@gmail.com>
2017-02-17 00:40:44 +0000

    Changed <= to < in #6 -- fence post problem.

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -12,10 +12,12 @@ void print_line(int n)
 	if (n < 0)
 	{
 		_putchar('\n');
+		_putchar('T');
 	}
 	else
-		for (i = 0; i <= n; i++)
+	{
+		for (i = 0; i < n; i++)
 			_putchar('_');
 		_putchar('\n');
-
+	}
 }
