0af1898563513fc83535cbea1d61bc3126dd860b
Larry Madeo <lmadeo76@gmail.com>
2017-02-17 01:09:33 +0000

    1st commit: 7-print_diagonals

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print diagonal line using '\' characters
+ * @n: int value how long (vertically) to make diagonal line
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j =1; j < i; j++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
