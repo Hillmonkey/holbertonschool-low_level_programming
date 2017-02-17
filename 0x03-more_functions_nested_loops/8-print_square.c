4b273c455addb6150a7e2f0ee4d216b3d28a6bd4
Larry Madeo <lmadeo76@gmail.com>
2017-02-17 06:49:44 +0000

    #8: I have harnessed the power of the goto statement for the first time in many years, and Betty let it slide. Ha ha hagit add 8-print_square.c git add 8-print_square.c git add 8-print_square.c git add 8-print_square.c

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -9,10 +9,16 @@ void print_square(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+		goto END;
+	}
 	for (i = 1; i <= n; i++)
 	{
 		for (j = 1; j <= n; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
+END:;
 }
