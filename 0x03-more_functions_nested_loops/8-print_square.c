b75a69721f2b809953e594afde60d4807de4ad5e
Larry Madeo <lmadeo76@gmail.com>
2017-02-17 06:31:58 +0000

    first commit: #8, output looks good to a drunk man.

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print diagonal line using '\' characters
+ * @n: int value how long (vertically) to make diagonal line
+ * Return: void
+ */
+void print_square(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
