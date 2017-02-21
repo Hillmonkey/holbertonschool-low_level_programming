6d6039bbf9f3479f7e08dc3b29cd7d31ab79751b
Larry Madeo <lmadeo76@gmail.com>
2017-02-21 00:42:35 +0000

    First commit 0, + readme file

---
+++ 0x04-pointer_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - accepts a pointer to an int,
+ *  and update the value it points to to 98
+ * @*n: pointer to an int
+ * Return: void
+ *		
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x04-pointer_arrays_strings/README.md
@@ -0,0 +1,3 @@
+### 0x04. C - pointers, arrays and strings
+
+A bunch of short C programs to help me extend my knowledge of C!  #cisfun
