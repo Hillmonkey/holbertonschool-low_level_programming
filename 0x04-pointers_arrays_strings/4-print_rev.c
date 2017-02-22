354a3cb534217fd04c500f9af5e7f682d57400d0
Larry Madeo <lmadeo76@gmail.com>
2017-02-22 06:10:57 +0000

    another push for the glory!

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_rev - Prints a string in reverse, followed by newling
+ * print_rev - Prints a string in reverse, followed by newline
  * @s : pointer to the beginning of a string
  * Return: void
  **/
@@ -9,7 +9,7 @@ void print_rev(char *s)
 {
 	int len = 0;
 
-	while (*s != 0)
+	while (*s !='\0')
 	{
 		len++;
 		s++;
