44659af48d60a78f3f7d8323cec9200cf199ac3f
Larry Madeo <lmadeo76@gmail.com>
2017-02-23 23:38:50 +0000

    removed extraneous comments from script #1

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,11 @@
 char *_strcat(char *dest, char *src)
 {
 	char *tmp1, *tmp2;
-	/* edge cases??? */
 	tmp1 = dest;
 	tmp2 = src;
-	/* wind out to end dest */
 	while (*dest != '\0')
-	{
 		dest++;
-	}
-	/* peal off trailing null byte */
 
-	/* loop through src */
 	while (*src != '\0')
 	{
 		*dest = *src;
@@ -30,7 +24,4 @@ char *_strcat(char *dest, char *src)
 	dest = tmp1;
 	src = tmp2;
 	return (dest);
-		/* copy each char from src to the end of dest */
-	/* ensure that terminating null byte exists */
-	/* return pointer to dest */
 }
--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -1,45 +1,23 @@
 #include "holberton.h"
 
-/**
- * _strlen - returns the length of a string
- * @s: pointer to a char (also functions as pointer to
- *    a string, which is an array of chars
- * Return: int representing the len of the string s
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s != 0)
-	{
-		i++;
-		s++;
-	}
-	return (i);
-}
-
-/**
- *_strncat - appends first n chars of src string to dest string
- *@dest: a pointer to a string
- *@src: a pointer to another string
- *@n: int that indicates how man char from src string to append
- * Return: a pointer to resulting string, dest
- */
 char *_strncat(char *dest, char *src, int n)
 {
 	char *tmp1, *tmp2;
-	/* edge cases??? */
+	int i;
+
 	tmp1 = dest;
 	tmp2 = src;
-	int i;
+
 	while (*dest != '\0')
 		dest++;
 	/* loop through src, til null or n chars  */
+	i = 0;
 	while (*src != '\0' && i < n)
 	{
 		*dest = *src;
 		dest++;
 		src++;
+		i++;
 	}
 	*dest = '\0';
 	dest = tmp1;
