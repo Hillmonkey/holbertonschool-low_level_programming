8572bd6ef55252dfd9f1c08644c24b092fa12c29
Larry Madeo <lmadeo76@gmail.com>
2017-02-24 21:14:21 +0000

    1st commit script #8: rot 13 script.

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * rot13 - converts string to rot13
+ * @s: string to be converted to rot encoding
+ * Return: pointer to string converted to rot13
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
