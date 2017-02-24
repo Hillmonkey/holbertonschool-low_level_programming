1b2a1cdfd4f29c8d4aca27467f5c30b71af1b3df
Larry Madeo <lmadeo76@gmail.com>
2017-02-24 04:24:45 +0000

    script #4: first commit.

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _reverse_array -  accepts array of ints and reverses them in place
+ * @a: pointer to array of ints
+ * @n: int representing size of array 'a'
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int tmp;
+
+	while (i < n / 2)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+		i++;
+	}
+}
