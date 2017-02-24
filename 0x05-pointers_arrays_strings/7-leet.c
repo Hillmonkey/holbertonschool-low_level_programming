48c77391a2063e395b30ad5f181e200ad6de7ca6
Larry Madeo <lmadeo76@gmail.com>
2017-02-24 19:18:39 +0000

    removed helper function, because it increased the number of if statements used, which is against specs.

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,15 +1,5 @@
 #include "holberton.h"
-/**
- * to_upper - if an ascii value is a lower, convert to upper
- * @c: char to be converted to upper, if appropriate
- * Return: char that has been capitalized, if appropriate
- */
-char to_upper(char c)
-{
-	if (c >= 97 && c <= 122)
-		c -= 32;
-	return (c);
-}
+
 /**
  * leet - convert string to leet talk
  * @s: string to be converted to leet talk
@@ -19,13 +9,13 @@ char *leet(char *s)
 {
 	int i = 0;
 	int j = 0;
-	static char ch[5] = {'A', 'E', 'O', 'T', 'L'};
-	static char loot[5] = {'4', '3', '0', '7', '1'};
+	char ch[10] = "aeotlAEOTL";
+	char loot[10] = "4307143071";
 
 	while (s[i] != 0)
 	{
-		for (j = 0; j < 5; j++)
-			if (to_upper(s[i]) == ch[j])
+		for (j = 0; j < 10; j++)
+			if (s[i] == ch[j])
 				s[i] = loot[j];
 		i++;
 	}
