0b8fc4b6c60d205e7e9df4f5f50e436b04585f7c
Larry Madeo <lmadeo76@gmail.com>
2017-02-27 07:25:52 +0000

    condensed snippet for blog post. Prob more condensing to come!

---
+++ 0x05-pointers_arrays_strings/BLOG/short-as-hell.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char c[] = {1, 2};
+
+	printf("%d\n", sizeof(c));
+	printf("%ld\n",sizeof(c[0]));
+	printf("%ld\n",sizeof(0));
+	printf("%d\n",sizeof(0));
+
+	printf("%ld\n", &c[0]);
+	printf("%ld\n", &0[c]);
+
+	return(0);
+}
