0707575bd925b887443138341b72b1a45e017925
Larry Madeo <lmadeo76@gmail.com>
2017-02-28 04:18:45 +0000

    coded script #2 in yet another way that seems to function the same.  Will check the autograder now ...

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,11 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	while ((s[i] != c) && (s[i] != 0))
+	while (s[i] != 0)
+	{
+		if (s[i] == c)
+			return (&s[i]);
 		i++;
-	if (s[i] == 0)
-		return (0);
-	else
-		return (&s[i]);
+	}
+	return (0);
 }
