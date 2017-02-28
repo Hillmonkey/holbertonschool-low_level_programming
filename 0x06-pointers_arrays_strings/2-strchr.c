bf6a4414c27e1940208fd11c4e434e591ffe9072
Larry Madeo <lmadeo76@gmail.com>
2017-02-28 03:48:36 +0000

    1st commit: script #2, strchr function

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to a string
+ * @c: an ascii character
+ * Return: pointer to first occurence of char c in string s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (!((s[i] == c) || (s[i] == 0)))
+		i++;
+	if (s[i] == 0)
+		return (0);
+	else
+		return (&s[i]);
+}
