b49b4ef3adb9e77c89d5161937794851f7c4aa81
Larry Madeo <lmadeo76@gmail.com>
2017-03-01 08:08:16 +0000

    1st commit: script #9, redirection confusion

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to a pointer to a char 
+ * @to: pointer to a char
+ * Returns: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
