ca89d41df7e6c1a2f8d28e5c8475eb37d3a998b5
Larry Madeo <lmadeo76@gmail.com>
2017-03-06 05:11:26 +0000

    3-mul.c has been overhauled, using arrays instead of pointers and ditching uneccesary for loop

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - prints number of arguments passed in
+ * @argc: int value representing number of arguments passed in
+ * @argv: pointer to an array of pointers to strings that
+ *        contain arguments passed in
+ * Return: 0 for success
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	if (argc != 3)
+	{
+		printf("argc = %d\n", argc);
+		puts("Error");
+		return (1);
+	}
+	else
+	{
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
+		return (EXIT_SUCCESS);
+	}
+}
