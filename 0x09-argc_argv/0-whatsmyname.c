e36818911289797c337a5eebe3cc2cfbbe4e5010
Larry Madeo <lmadeo76@gmail.com>
2017-03-06 02:16:14 +0000

    updated script #0 to appease Betty checker

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+ * main - returns first parameter passed at command line
+ * @argc: number of variables passed at command line
+ * @argv: pointer to a list of pointers, that are at heads of strings
+ * Return: int (zero) on success
+ */
+
 int main(int argc, char **argv)
 {
 	argc = 0;
