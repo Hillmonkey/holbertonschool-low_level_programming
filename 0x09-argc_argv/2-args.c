84bdd656eabed8f757b487591f0bf6e3da642e2c
Larry Madeo <lmadeo76@gmail.com>
2017-03-06 04:14:25 +0000

    fixed script #2: used __attribute__((unused)) instead of hack, and changed while test to arg-- to evaluate after decrement.

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 /**
- * main - prints number of arguments passed in
+ * main - prints all arguments passed in
  * @argc: int value representing number of arguments passed in
  * @argv: pointer to an array of pointers to strings that
  *        contain arguments passed in
  * Return: 0 for success
  */
-int main(int argc, char **argv)
+int main(int argc, char __attribute__((unused)) **argv)
 {
-	int i = 1;
-
-	while (--argc)
-		printf("%s\n", *argv++);
-		i *= atoi(*argv++);
+	while (argc--)
+		printf("%s\n", *argv);
 	return (EXIT_SUCCESS);
 }
