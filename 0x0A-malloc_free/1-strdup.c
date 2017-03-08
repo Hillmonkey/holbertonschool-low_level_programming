187a204b8588dca5da498a41dd44d4053730ee1c
Larry Madeo <lmadeo76@gmail.com>
2017-03-08 02:22:18 +0000

    1st commit of 1-strdup.c, wish me luck!

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to a char (also functions as pointer to
+ *     a string, which is an array of chars
+ * Return: int representing the len of the string s
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	char *tmp;
+
+	tmp = s;
+	while (*s != 0)
+	{
+		i++;
+		s++;
+	}
+	s = tmp;
+	return (i);
+}
+/**
+ * _strdup - duplicate a string and place it in newly allocated space in
+ *          memory
+ * @str: string to be copied
+ * Return: pointer to duplicated string, or NULL if sufficient memory
+ *         is not available
+ */
+char *_strdup(char *str)
+{
+	char *new;
+	unsigned int i, size;
+
+	size = _strlen(str);
+	new = malloc(size * sizeof(char));
+	if (new == NULL || size == 0 || *str == '\0')
+		return (NULL);
+	for (i = 0; i < size; i++)
+		new[i] = str[i];
+	return (new);
+}
