f7787ad94b3f6578a7f07d7583f4188ed2fd4606
Larry Madeo <lmadeo76@gmail.com>
2017-03-09 17:20:01 +0000

    incomplete solution for #5, saved for future extension

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+char *argstostr(int ac, char **av)
+{
+	char *bigstr;
+
+	if ((ac == 0) || (av == NULL))
+		return (NULL);
+	/* determine bytes to be malloc'ed */
+	myLen = (ac + 1 + total_len);
+	if ( )
+
+	
+}
