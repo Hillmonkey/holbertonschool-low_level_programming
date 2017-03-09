84545e57a587849361dbfd236c0515c199cd5eca
Larry Madeo <lmadeo76@gmail.com>
2017-03-09 01:28:28 +0000

    Fixed script #4: included more descriptive comment fields betwixt includes and body of function ...

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * 
- *
- *
+ * free_grid - totally deallocate 2d array created in proj #3
+ * @grid: pointer to an array of ints
+ * @height: int representin # of rows in 2d array
  */
 void free_grid(int **grid, int height)
 {
-	int ,h; 
-	
+	int h;
+
 	h = 0;
 	while (h < height)
 	{
