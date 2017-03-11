f6493f7dcbec702f47a9133a98177e53a640578f
Larry Madeo <lmadeo76@gmail.com>
2017-03-11 05:51:32 +0000

    faster_calloc functions the same as 2-calloc, but pushes data 8 bytes at a time.

---
+++ 0x0B-more_malloc_free/faster_calloc.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * faster_calloc - this function pushes 0s to malloc'ed memory, 8 bytes at
+ *                 at a time!
+ * @nmemb: the number of array members to make space for
+ * @size:  the number of bytes in each array member
+ * Return
+ */
+void *faster_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
