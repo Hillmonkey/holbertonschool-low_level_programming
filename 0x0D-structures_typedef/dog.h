38d08d7aa5bdefa736057d1c6c08cdb58ab4a686
Larry Madeo <lmadeo76@gmail.com>
2017-03-14 19:30:06 +0000

    updated header file, with function prototype for free-dogs script

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -8,18 +8,17 @@
  * @owner: name of dog owner
  * Descripton: Everyone wants dog info!
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
