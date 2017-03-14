20dad1d7175b6858d1791b6e72d499a143d94957
Larry Madeo <lmadeo76@gmail.com>
2017-03-14 03:03:02 +0000

    first commit for script #1

---
+++ 0x0D-structures_typdef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initiales dog struct
+ * @d: pointer to an instance of dog struct
+ * @name: string representing dog's name
+ * @age: float representing dog's age
+ * @owner: string representing dog owner's name
+ **/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
