fe4ec3be6c3adc82b71f8746532042cdc2367196
Larry Madeo <lmadeo76@gmail.com>
2017-03-14 08:08:47 +0000

    updated script #4 so that copies are made of strings, not copies of pointers.  Also space is malloc'ed for string elements of struct, and tested each time for return value of malloc function.

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,46 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - returns the length of a string
+ * @s: pointer to a char (also functions as pointer to
+ *    a string, which is an array of chars
+ * Return: int representing the len of the string s
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	char *tmp;
+
+	tmp = s;
+	while (*s != 0)
+	{
+		i++;
+		s++;
+	}
+	s = tmp;
+	return (i);
+}
+
+/**
+ * _strcpy - copy string from src to dest
+ * @dest: destination string
+ * @src: source string
+ * Return: Copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	dest[count] = '\0';
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: dog name
  * @age: dog age in years (float)
@@ -16,9 +56,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	this_dog = malloc(sizeof(dog_t));
 	if (this_dog == NULL)
 		return (NULL);
-	this_dog->name = name;
+
+	this_dog->name = malloc(1 + _strlen(name));
+	if (this_dog->name == NULL)
+		return (NULL);
+	this_dog->name = _strcpy(this_dog->name, name);
+
 	this_dog->age = age;
-	this_dog->owner = owner;
+
+	this_dog->owner = malloc(1 + _strlen(owner));
+	if (this_dog->owner == NULL)
+		return (NULL);
+	this_dog->owner = _strcpy(this_dog->owner, owner);
 
 	return (this_dog);
 }
