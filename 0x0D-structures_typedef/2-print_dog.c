111814d59e292ba2338d29f8fd8424dfa4c4b7d0
Larry Madeo <lmadeo76@gmail.com>
2017-03-14 05:36:34 +0000

    first commit script #2, print my struct!

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints struct dog!!!
+ *
+ *
+ **/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: ");
+	if (d->name != NULL)
+		printf("%s\n", d->name);
+	else
+		puts("(nil)");
+
+	printf("Age : ");
+	if (&d->age == NULL)
+		printf("(nil)\n");
+	else
+		printf("%.6f\n", d->age);
+
+	printf("Owner: ");
+	if (d->name != NULL)
+		printf("%s\n", d->owner);
+	else
+		puts("(nil)");
+}
