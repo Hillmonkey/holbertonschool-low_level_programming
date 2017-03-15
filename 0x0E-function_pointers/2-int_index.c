b33530c3ee8f5f1c95cbdd6744871e2dc738038f
Larry Madeo <lmadeo76@gmail.com>
2017-03-15 21:12:59 +0000

    script #2: added test for array == NULL

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int x = 0;
 
-	if (size <= 0 || cmp == NULL)
+	if (size <= 0 || cmp == NULL || array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
