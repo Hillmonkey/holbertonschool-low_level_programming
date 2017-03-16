675ef533bd7b2a9aac3192e2d27b4d9bbe583fac
Larry Madeo <lmadeo76@gmail.com>
2017-03-16 06:44:25 +0000

    changed around 3-main.c and back as my understanding evolved ...

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,15 +1,20 @@
-
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc,char *argv[])
 {
+	
 	int a;
 	
+	printf("2argc = %d\n", argc);
 	if (argc != 4)
 	{
-		printf("error\n")
+		printf("argc = %d\n", argc);
+		printf("error-A-\n");
 		exit (98);
 	}
-	a = (*get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+	a = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", a);
 	return (0);
 }
