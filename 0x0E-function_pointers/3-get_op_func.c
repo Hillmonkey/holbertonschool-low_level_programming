1bb439a39eb247b26c8ebc09a1581edbecf5d0f5
Larry Madeo <lmadeo76@gmail.com>
2017-03-16 20:56:40 +0000

    3-get_op_func.c -> modified while loop conditional to test for null, instead of depending on the length of the array of structs.

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -19,12 +19,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 6)
+	while (ops[i].op != NULL)
 	{
 		if ((strcmp((ops[i].op), s)) == 0)
 			return (ops[i].f);
 			i++;
 	}
-	return (NULL);
+	puts("Error");
+	exit(99);
 }
 
