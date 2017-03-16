528a34a90985b762c2d902c67fe3f6a06d223af5
Larry Madeo <lmadeo76@gmail.com>
2017-03-16 19:52:44 +0000

    function_pointers.h: changed from use of stddef.h to typedef of size_t

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef FPTR
 #define FPTR
 
-#include <stddef.h>
+typedef unsigned long int size_t;
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
