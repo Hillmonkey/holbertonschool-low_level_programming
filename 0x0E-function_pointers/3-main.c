55831becd83f1d19b3f782fe92c90f64e0e7b724
Larry Madeo <lmadeo76@gmail.com>
2017-03-16 07:11:29 +0000

    3-main.c now has a proper description, also various extraneous spaces have been removed from source.

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,16 +1,21 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(int argc,char *argv[])
+/**
+ * main - accepts input to decide what type of arithmetic function to call
+ * @argc: int representing # of command line args
+ * @argv: an array of strings, passed to main at the command line
+ * Return: int, being product of operator on ints a & b
+ **/
+int main(int argc, char *argv[])
 {
-	
+
 	int a;
-	
+
 	if (argc != 4)
 	{
 		printf("error\n");
-		exit (98);
+		exit(98);
 	}
 	a = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", a);
