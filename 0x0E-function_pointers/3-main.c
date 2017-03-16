d9befd44320bef4b3325019bbfbcef5c069f954d
Larry Madeo <lmadeo76@gmail.com>
2017-03-16 06:59:15 +0000

    remove debugging printf's

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -7,11 +7,9 @@ int main(int argc,char *argv[])
 	
 	int a;
 	
-	printf("2argc = %d\n", argc);
 	if (argc != 4)
 	{
-		printf("argc = %d\n", argc);
-		printf("error-A-\n");
+		printf("error\n");
 		exit (98);
 	}
 	a = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
