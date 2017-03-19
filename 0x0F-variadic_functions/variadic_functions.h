3a98023090a4cac498115561c64f43347fd61736
Larry Madeo <lmadeo76@gmail.com>
2017-03-19 23:46:36 +0000

    Modified variadic header so that struct.op doesn't specify variable types being passed in. Also, 2-print-strings.c  has been removed because 2-print_strings.c is the desired file name.

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
 
+/**
+ * struct pf - Struct p(rint) f(ormat)
+ * @fmt: single char string (but could be longer) specifying something similar
+ *       to printf formatting
+ * @f: function associated with "printf" format flag
+ **/
+typedef struct pf
+{
+	char *fmt;
+	char *(*f)()
+} pa;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
