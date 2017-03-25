593e19d54b94b2ae3b548ab1e1fea631d7fa8887
Larry Madeo <lmadeo76@gmail.com>
2017-03-25 08:11:14 +0000

    Wow. if statement @ line 28 caused segfault until I dereferenced head. Other pointer fixes got this little beast off the ground

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -12,34 +12,27 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	putchar('\n');
-	puts("A\n");
 	list_t *new;
 	list_t *tmp;
 	unsigned int i;
 
-	puts("B\n");
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	puts("C\n");
 	new->str = strdup(str);
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	new->len = i;
-	new->next = NULL;
-	puts("F\n");
-	printf("%p\n", *head);
-	tmp = *head;
-	printf("%d", *tmp->str);
-	
-	puts("1");
-	puts(tmp->str);
-	while (tmp->next != NULL)
+	new->next = NULL; /* ready to be new tail */
+
+	if (*head == NULL)
 	{
-		puts("G");	
+		*head = new;
+		return(*head);
+	}	
+	tmp = *head;
+	while (tmp->next != NULL)
 		tmp = tmp->next;
-	}
 	tmp->next = new;
 	return (new);
 }
