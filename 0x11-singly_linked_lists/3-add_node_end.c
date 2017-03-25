1b1cb9ec323e86c0ae1ed96607b184fbd4d9b92f
Larry Madeo <lmadeo76@gmail.com>
2017-03-25 05:30:09 +0000

    add-node script with debugging features.

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -4,28 +4,42 @@
 #include "lists.h"
 
 /**
- * add_node - add new node at end of a list_t list
+ * add_node_end - add new node at end of a list_t list
  * @head: pointer to a pointer to a list element
  * @str: string element of node
  * Return: address of new element, or NULL if failed
  **/
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
+	putchar('\n');
+	puts("A\n");
 	list_t *new;
+	list_t *tmp;
 	unsigned int i;
 
+	puts("B\n");
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+	puts("C\n");
 	new->str = strdup(str);
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	new->len = i;
 	new->next = NULL;
-	/* got to get to the end of the list and append */
-	/* new->next = *head; */
-	*head->next = *new;
-
+	puts("F\n");
+	printf("%p\n", *head);
+	tmp = *head;
+	printf("%d", *tmp->str);
+	
+	puts("1");
+	puts(tmp->str);
+	while (tmp->next != NULL)
+	{
+		puts("G");	
+		tmp = tmp->next;
+	}
+	tmp->next = new;
 	return (new);
 }
