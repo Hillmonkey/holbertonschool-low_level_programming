25ba2cc704d62b053b24e69fa6255b04669822da
Larry Madeo <lmadeo76@gmail.com>
2017-03-29 22:36:14 +0000

    moved add_nodeint_end into insert_nodeint.c file

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -46,7 +46,34 @@ listint_t *join(listint_t *h1, listint_t *h2)
 	tmp->next = h2;
 	return (h1);
 }
+/**
+ * add_nodeint_end - add new node at end of a listint_t list
+ * @head: pointer to a pointer to a head list node
+ * @n: int value to store in a new node
+ * Return: address of new element, or NULL if it failed
+ **/
 
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *tmp;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	tmp = *head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
+}
 /**
  * insert_nodeint_at_index - the name says it all!
  * @head: pointer to a pointer to head of linked list
