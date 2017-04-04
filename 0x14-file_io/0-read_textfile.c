8c1944265c70d86fb167ac93ff1ca151e174ccc7
Larry Madeo <lmadeo76@gmail.com>
2017-04-04 22:05:43 +0000

    somewhat reverted code that WOMC

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -9,21 +9,22 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t n2, n = 0;
+	ssize_t n = 0;
+	ssize_t n2 __attribute__((unused));
 	char *buf;
 
 	fd = open(filename, O_RDONLY);
 
-	if(fd == -1 || !filename)
+	if (fd == -1 || !filename)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
-		return (0);	
+		return (0);
 	n = read(fd, buf, letters);
 	buf[letters] = '\0';
 
-	printf("%s", buf); 
-	/* n2 = write(STDOUT_FILENO, buf, letters); */
+	/* printf("%s", buf); */
+	n2 = write(STDOUT_FILENO, buf, letters);
 	/* free(buf); */
-	return (n2);
+	return (n);
 }
