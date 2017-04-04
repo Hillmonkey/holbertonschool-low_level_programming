da916e538980912ef437a51903b50b2054a0f71b
Larry Madeo <lmadeo76@gmail.com>
2017-04-04 21:02:36 +0000

    added description, and fixed formatting

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -5,23 +5,28 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "holberton.h"
-
+/**
+ * read_textfile - reads and outputs a given number of characters of a text file
+ * @filename: string name of file to be worked with
+ * @letters: number of letters to be read
+ * Return: Number of characters actually printed, or 0 on an error
+ **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t n = 0;
-	char * buf;
+	char *buf;
 
 	fd = open(filename, O_RDONLY);
 
-	if(fd == -1 || !filename)
+	if (fd == -1 || !filename)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
-		return (0);	
+		return (0);
 	n = read(fd, buf, letters);
 	buf[letters] = '\0';
 
-	printf ("%s", buf);
+	printf("%s", buf);
 	return (n);
 }
