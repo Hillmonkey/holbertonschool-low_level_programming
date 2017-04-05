caab014bfb722b30d3f05199ddc6aefe6cd87c42
Larry Madeo <lmadeo76@gmail.com>
2017-04-05 22:42:15 +0000

    one more error msg typo now fixed!

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
-		dprintf(STDERR_FILENO, "Can't read from file %s", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]), exit(98);
 	fd2 = (open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664));
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
