ad158e1b65af674e066c0583442bec600d3ec3e2
Larry Madeo <lmadeo76@gmail.com>
2017-04-05 00:20:31 +0000

    tested for null pointer and empty string of filename

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -18,6 +18,8 @@ int create_file(const char *filename, char *text_content)
 	ssize_t fd;
 	ssize_t n;
 
+	if (filename == NULL || *filename == '\0')
+		return (-1);
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
