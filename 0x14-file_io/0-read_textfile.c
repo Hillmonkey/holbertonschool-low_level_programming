ef154638a92703d574b5a3c3b03c1ba822c7a423
Larry Madeo <lmadeo76@gmail.com>
2017-04-06 01:31:40 +0000

    may need to be uncommited later!

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -20,9 +20,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 	ssize_t tmp;
 
-	if (fd == -1 || !filename)
+	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
 	buf = malloc(sizeof(char) * (letters + 1));
 	if (!buf)
