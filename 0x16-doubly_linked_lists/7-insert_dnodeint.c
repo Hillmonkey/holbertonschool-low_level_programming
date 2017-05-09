a21caa5cdfbc665ba8f98222947a650e6911dc02
Larry Madeo <lmadeo76@gmail.com>
2017-05-09 01:22:35 +0000

    added add2end function

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,6 +4,38 @@
 #include "lists.h"
 
 /**
+ * add_dnodeint_end - add new node at end of doubly linked list list
+ * @head: pointer to a pointer to head list element
+ * @n: integer data to put in new list element
+ * Return: address of new element, or NULL on failure
+ **/
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *tmp;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	tmp = *head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+	new->prev = tmp;
+	return (*head);
+}
+
+/**
  * add_dnodeint - add new node at beginning of a dlistint_t list
  * @head: pointer to a pointer to a list element
  * @n: int data element of a node
