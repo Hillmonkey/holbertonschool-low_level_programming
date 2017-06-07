c487862e3f408610aa888386ededae4d8d842a80
Larry Madeo <lmadeo76@gmail.com>
2017-06-07 04:13:35 +0000

    mallocing chars instead of space for pointers

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -14,10 +14,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	if (size == 0)
+	if (size <= 0)
 		return (NULL);
 
-	array = malloc(sizeof(char *) * size);
+	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
