f465e16944d91f653f0f737b9723b17338805793
Larry Madeo <lmadeo76@gmail.com>
2017-06-08 21:06:33 +0000

    fixed header file

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -1,15 +1,2 @@
-/**
- * malloc_checked - allocate memory
- * @b: number of bytes to allocate
- * Return: pointer to allocated memory or 98 if malloc is unsuccesful
- **/
 
 void *malloc_checked(unsigned int b);
-{
-	void *ptr
-
-	ptr = malloc(b);
-	if (ptr = NULL)
-		exit (98);
-	return (ptr);
-}
