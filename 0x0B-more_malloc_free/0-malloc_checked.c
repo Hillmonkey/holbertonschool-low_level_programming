3686aa5e881843f86c1ebc757da8a0fd24fef9c7
Larry Madeo <lmadeo76@gmail.com>
2017-06-08 19:56:46 +0000

    first commit

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory of specified size
+ * @b: number of bytes to allocate
+  Return: pointer to allocated memory or 98 if malloc is unsuccesful
+ **/
+
+void *malloc_checked(usigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98;
+	return(ptr);
+}
---
+++ 0x0B-more_malloc_free/holberton.h
@@ -0,0 +1,15 @@
+/**
+ * malloc_checked - allocate memory
+ * @b: number of bytes to allocate
+ * Return: pointer to allocated memory or 98 if malloc is unsuccesful
+ **/
+
+void *malloc_checked(unsigned int b);
+{
+	void *ptr
+
+	ptr = malloc(b);
+	if (ptr = NULL)
+		exit (98);
+	return (ptr);
+}
