e872af7b374d5b9f745c05c65f27dfdb36e3e230
Larry Madeo <lmadeo76@gmail.com>
2017-06-12 20:38:33 +0000

    usually don't commit main.c, this is an exception

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include main(void)
+
+/**
+ * main - uses standard preprocessor macro to print file name of original
+ *        source file
+ * Return: void
+ **/
+
+int main(void)
+{
+	printf("%s\n, __FILE__);
+	return (0);
+}
+
