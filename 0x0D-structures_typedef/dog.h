a253b97ed861c1f354d9befd0ac4a79a842d7f9b
Larry Madeo <lmadeo76@gmail.com>
2017-06-13 05:31:05 +0000

    first commit

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog info
+ * @name: dog name
+ * @age: dog age in years, floats
+ * @owner: name of dog owner
+ * Descripton: Everyone wants dog info!
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif /* DOG_H */
