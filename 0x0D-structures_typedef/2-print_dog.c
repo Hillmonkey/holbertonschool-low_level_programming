5dfa147eefd122da967014ec4bbace7e62de035a
Larry Madeo <lmadeo76@gmail.com>
2017-06-13 05:42:26 +0000

    first commit

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints dog struct
+ * @d: pointer to instance of dog struct
+ * Return: void
+ **/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: ")
+	if (d->name != NULL)
+		printf("%s\n", d->name);
+	else
+		puts("(nil)");
+
+	printf("Age: %.6f\n", d->age);
+
+	printf("Owner: ");
+	if ((*d).name != NULL)
+		printf("%s\n", d->owner);
+	else
+		puts("(nil)");
+}
