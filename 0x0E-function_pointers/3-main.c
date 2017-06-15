8740bf0bce446297aaedfb7c2fb8253d74188c80
Larry Madeo <lmadeo76@gmail.com>
2017-06-15 07:03:55 +0000

    Revert "switched from atoi to strtol"
    This reverts commit cc5e4248ade52d756d88a18b355c575518b45a24.

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,17 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-int print_args(int ac, char *av[])
-{
-	int i = 0;
-
-	for (i = 0; i < ac; i++)
-		printf("argc[%d] --> %s\n", i, av[i]);
-	return (0);
-}
-
-
 /**
  * main - accepts input to decide what type of arithmetic function to call
  * @argc: int representing # of command line args
@@ -22,19 +11,13 @@ int main(int argc, char *argv[])
 {
 
 	int a;
-	long int b, c;
-	char *endptr;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		/* print_args(argc, argv); */
 		exit(98);
 	}
-	b = strtol(argv[1], &endptr, 10);
-	c = strtol(argv[3], &endptr, 10);
-	/* a = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])); */
-	a = (*get_op_func(argv[2]))(b, c);
+	a = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", a);
 	return (0);
 }
