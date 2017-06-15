93f85ba1f9c1a2eaf68a4525d245efa7fa6585f6
Larry Madeo <lmadeo76@gmail.com>
2017-06-15 06:14:37 +0000

    commiting 3-main.c, and modifying .gitignore

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - accepts input to decide what type of arithmetic function to call
+ * @argc: int representing # of command line args
+ * @argv: an array of strings, passed to main at the command line
+ * Return: int, being product of operator on ints a & b
+ **/
+int main(int argc, char *argv[])
+{
+
+	int a;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", a);
+	return (0);
+}
