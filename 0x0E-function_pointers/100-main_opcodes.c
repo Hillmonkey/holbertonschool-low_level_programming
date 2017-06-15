e99bea84c8fd64119899357babcbdb87b5d38eb5
Larry Madeo <lmadeo76@gmail.com>
2017-06-15 20:45:31 +0000

    first commit for the xtra credit!

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print out byte codes of itself
+ * @argc: number of args
+ * @argv: array of argument strings
+ * Return: int ... 0 = success, other numbers otherwise
+ **/
+
+int main(int argc, char *argv[])
+{
+	int (*f)(int, char**);
+	int i;
+	int num_bytes;
+	char *byte;
+
+	f = main;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	printf("num_bytes = %d\n", num_bytes);
+	for (i = 0; i < num_bytes; i++)
+	{
+		byte = *(f + i);
+		printf("%02hhx", (unsigned char) *byte);
+		if (i == num_bytes - 1)
+			puts("");
+		else
+			printf(" ");
+	}
+	return (0);
+}
