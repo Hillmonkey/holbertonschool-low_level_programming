f3b40d23e036585e07b8d65fdd689d1f86c95ad2
Larry Madeo <lmadeo76@gmail.com>
2017-06-17 22:32:17 +0000

    first commit

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,78 @@
+#include "variadic_functions.h"
+
+/**
+ * op_char - print output based on variadic input
+ * @valist: tool to grab next item out of variable length list
+ **/
+void op_char(va_list valist)
+{
+	printf("%c", va_arg(valist, int));
+}
+
+/**
+ * op_int - print output based on variadic input
+ * @valist: tool to grab next item out of variable length list
+ **/
+void op_int(va_list valist)
+{
+	printf("%d", va_arg(valist, int));
+}
+
+/**
+ * op_float - print output based on variadic input
+ * @valist: tool to grab next item out of variable length list
+ **/
+void op_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+
+/**
+ * op_string - print output based on variadic input
+ * @valist: tool to grab next item out of variable length list
+ **/
+void op_string(va_list valist)
+{
+	char *str;
+
+	str = va_arg(valist, char*);
+	if (str)
+		printf("%s", str);
+	else
+		printf("(nil)");
+}
+/**
+ * print_all - this function prints anything!!!
+ * @format: string containing list of arg types to pass
+ **/
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	int i = 0, j = 0;
+	op_t ops[] = {
+		{"c", op_char},
+		{"i", op_int},
+		{"f", op_float},
+		{"s", op_string},
+		{NULL, NULL}
+	};
+
+	va_start(valist, format);
+	while (format[i]) /* "ceis" */
+	{
+		j = 0;
+		while (ops[j].f)
+		{
+			if (ops[j].op[0] == format[i])
+			{
+				ops[j].f(valist);
+				if (!(format[i] == '\0' || format[i + 1] == '\0'))
+					printf(", ");
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+}
