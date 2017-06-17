f4cf481d98415d5327a60ab17c97975a2fc98cf8
Larry Madeo <lmadeo76@gmail.com>
2017-06-17 22:33:07 +0000

    added typedef and op-functions

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -3,6 +3,24 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * struct op - Struct op
+ * @op: printf-like operator
+ * @f: formatting function
+ **/
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list);
+} op_t;
+
+/* printf op functions */
+void op_char(va_list);
+void op_int(va_list);
+void op_float(va_list);
+void op_string(va_list);
+
+/* other functions */
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
