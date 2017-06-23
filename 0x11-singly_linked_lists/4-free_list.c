4d952931f5e07ae35ecab0191932e87ca0749818
Larry Madeo <lmadeo76@gmail.com>
2017-06-23 02:31:42 +0000

    again ...
    dealing with changes to a singly dereferenced double pointer
    which has scope beyond the local function, thus necessitating
    the use of a tmp variable, so as to not mess with the state of
    the variable beyond the function

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -6,7 +6,6 @@
 /**
  * r_free_list - frees malloc'ed memory recursively - one link at a time.
  * @ptr: pointer that works its way through a list
- * Return: if ptr != NULL, return ptr->next
  **/
 void r_free_list(list_t *ptr)
 {
@@ -14,21 +13,16 @@ void r_free_list(list_t *ptr)
 		r_free_list(ptr->next);
 	free(ptr->str);
 	free(ptr);
-	return;
 }
 
 
-/** 
+/**
  * free_list - frees all malloc'ed memory associated with a linked list
- * @:head: pointer to head of linked list
+ * @head: pointer to head of linked list
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
-
 	if (head == NULL)
 		return;
-	temp = head;
-	r_free_list(temp);
-	return;
+	r_free_list(head);
 }
