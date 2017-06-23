62ef93014e293dfab54a168ef919c1b973607555
Larry Madeo <lmadeo76@gmail.com>
2017-06-23 02:44:13 +0000

    first commit

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+/**
+ * premain - prints string before main executes
+ **/
+
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
