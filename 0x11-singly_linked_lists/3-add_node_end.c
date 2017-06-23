f6583459f735714b68b69f081152892927341d13
Larry Madeo <lmadeo76@gmail.com>
2017-06-23 02:31:11 +0000

    format issues

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	unsigned int len;
 	list_t *new, *tmp;
 
-	tmp = *head;	
+	tmp = *head;
 	if (!head)
 		return (NULL);
 	new = malloc(sizeof(list_t));
