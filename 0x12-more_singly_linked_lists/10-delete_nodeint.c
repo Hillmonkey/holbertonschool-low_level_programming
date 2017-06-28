bca68f9c0831a36bf384d14bd24b9a285768de08
Larry Madeo <lmadeo76@gmail.com>
2017-06-27 19:06:26 -0700

    removed duplicative logic

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,10 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			if (tmp != NULL)
 				tmp = tmp->next;
 		}
-		if (prev->next->next)
-			prev->next = prev->next->next;
-		if (index == len)
-			prev->next = NULL;
+		prev->next = prev->next->next;
 		if (tmp)
 		{
 			free(tmp);
