07f505ebe9ae3e7f21543b991ccf62215a840dbc
Larry Madeo <lmadeo76@gmail.com>
2017-07-03 23:29:41 +0000

    first commit

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - output contents of text file to STDOUT
+ * @filename: string name of file (tested in CWD)
+ * @letters: length of file in chars
+ * Return: actual number of chars printed
+ **/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd;
+	ssize_t n = 0, n2;
+	char *buf;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (!buf)
+		return (0);
+	n = read(fd, buf, letters);
+	if (n == -1)
+		return (0);
+	/*tmp = n; */
+
+	buf[n] = '\0';
+
+	n2 = write(STDOUT_FILENO, buf, n + 1);
+	if (n2 == -1)
+		return (0);
+	if (close(fd) < 0)
+		return (0);
+	free(buf);
+	return (n);
+}
---
+++ 0x14-file_io/README.md
@@ -0,0 +1,7 @@
+# 0x14. C - File I/O
+
+### Learning to code with [Larry Madeo](https://twitter.com/larmalade)
+
+### [Holberton School](https://www.holbertonschool.com) assignment solutions - July, 2017
+
+Scripts executing file I/O tasks for self-eductation.
