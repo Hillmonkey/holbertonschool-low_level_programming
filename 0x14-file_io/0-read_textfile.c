7545d1d153343788d41e6bfd1ceced13e2f72d65
Larry Madeo <lmadeo76@gmail.com>
2017-07-03 21:09:43 -0700

    more closing fd at exit points`

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -17,19 +17,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		close(fd);
 		return (0);
-
+	}
 	buf = malloc(sizeof(char) * (letters + 1));
 	if (!buf)
+	{
+		close(fd);
 		return (0);
+	}
 	n = read(fd, buf, letters);
 	if (n == -1)
 	{
 		free(buf);
+		close(fd);
 		return (0);
 	}
 	buf[n] = '\0';
-
 	n2 = write(STDOUT_FILENO, buf, n + 1);
 	if (n2 == -1)
 	{
