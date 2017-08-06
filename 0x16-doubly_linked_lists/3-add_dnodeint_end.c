27c02daa21c052547b50a22fff173e62a90e4469
Larry Madeo <lmadeo76@gmail.com>
2017-08-06 01:12:40 +0000

    removed use of previ pointer'
    :

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *tmp, *previ;
+	dlistint_t *new, *tmp;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -27,13 +27,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 
 	tmp = *head;
-	previ = *head;
 	while (tmp->next != NULL)
 	{
-		previ = tmp;
 		tmp = tmp->next;
 	}
 	tmp->next = new;
-	new->prev = previ;
+	new->prev = tmp;
 	return (new);
 }
