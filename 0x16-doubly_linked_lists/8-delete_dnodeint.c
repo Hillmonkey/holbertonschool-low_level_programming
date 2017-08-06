fadd71167bbbf1f90ee92421d9b16a72b7df1a9b
Larry Madeo <lmadeo76@gmail.com>
2017-08-06 01:38:04 +0000

    test for *head != NULL

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp;
 	unsigned int i;
 
-	if (!head)
+	if (!head || !(*head))
 		return (-1);
 	tmp = *head;
 	for (i = 0; tmp && i <= index; i++, tmp = tmp->next)
