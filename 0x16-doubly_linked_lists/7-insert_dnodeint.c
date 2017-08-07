c3285100fdffdbe4e523482902e49bdf3620ef1d
Larry Madeo <lmadeo76@gmail.com>
2017-08-07 00:41:16 +0000

    semi-colon to comma for formating

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t *));
 	if (!new)
 		return (NULL);
-	new->n = n, new->next = NULL; new->prev = NULL;
+	new->n = n, new->next = NULL, new->prev = NULL;
 	if (*h == NULL)
 	{
 		*h = new;
