882a307fc4e855aae7e9ac579a443d618e517e4b
Larry Madeo <lmadeo76@gmail.com>
2017-09-02 00:20:46 +0000

    pulled insert_node func into 3-hash....c file

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,6 +1,38 @@
 #include "hash_tables.h"
 
 /**
+ * insert_node - insert node into hash table
+ * @ht: pointer to hash table data structure
+ * @h_node: already created node to insert into hash table
+ * Return: 1 on success, 0 on failure
+ **/
+int insert_node(hash_table_t *ht, hash_node_t *h_node)
+{
+	unsigned long int idx;
+	hash_node_t *tmp;
+
+	/* TABLE_SIZE should be same as ht->size */
+	idx = key_index((const unsigned char *)(h_node->key), TABLE_SIZE);
+	tmp = ht->array[idx];
+	while (tmp)
+	{
+		/* test for key clash */
+		if (strcmp(tmp->key, (const char *)(h_node->key)) == 0)
+			return (EXIT_FAILURE);
+		tmp = tmp->next;
+	}
+	if (ht->array[idx] == NULL)
+	{
+		ht->array[idx] = h_node;
+		return (EXIT_SUCCESS);
+	}
+	tmp = ht->array[idx];
+	ht->array[idx] = h_node;
+	h_node->next = tmp;
+	return (EXIT_SUCCESS);
+}
+
+/**
  * hash_table_set - add key value pair to hash table
  * @ht: pointer to hash table data structure
  * @key: key string (must be unique)
