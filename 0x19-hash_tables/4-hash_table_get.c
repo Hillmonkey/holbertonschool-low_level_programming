9d3286429d8e1319c93c71bce0cc1c8836f372f7
Larry Madeo <lmadeo76@gmail.com>
2017-09-05 08:10:11 +0000

    got rid of reference to TABLE_SIZE macro

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -19,16 +19,16 @@ const char *grab_value(hash_node_t *HN, const char *key)
 
 /**
  * hash_table_get - retrieve value associated with a key
- * @ht: pointer to a hash table
+ * @HT: pointer to a hash table
  * @key: key string
  * Return: value string
  **/
-char *hash_table_get(const hash_table_t *ht, const char *key)
+char *hash_table_get(const hash_table_t *HT, const char *key)
 {
 	unsigned long int idx;
 	/* unsigned long int hash; */
 
-	idx = key_index((const unsigned char *)key, TABLE_SIZE);
+	idx = key_index((const unsigned char *)key, HT->size);
 	/* hash = hash_djb2((const unsigned char *)key); */
-	return ((char *)(grab_value((ht->array[idx]), key)));
+	return ((char *)(grab_value((HT->array[idx]), key)));
 }
