e4eebebb81ae06780f167ac5a382ce50ca4895a2
Larry Madeo <lmadeo76@gmail.com>
2017-10-26 22:25:08 -0700

    now tests for null parent before mallocing

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -10,14 +10,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node, *old_left_node = NULL;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent != NULL)
-		old_left_node = parent->left;
-	else
-		return (NULL);
+	old_left_node = parent->left;
 
 	new_node->n = value;
 	new_node->right = NULL;
