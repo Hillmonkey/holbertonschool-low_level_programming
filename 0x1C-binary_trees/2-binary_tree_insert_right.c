c57304654e78e73eaea2410f196e207a52829bce
Larry Madeo <lmadeo76@gmail.com>
2017-10-26 22:31:03 -0700

    now handles parent = NULL

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -10,12 +10,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node, *old_right_node = NULL;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent != NULL)
-		old_right_node = parent->right;
+	old_right_node = parent->right;
 
 	new_node->n = value;
 	new_node->right = NULL;
