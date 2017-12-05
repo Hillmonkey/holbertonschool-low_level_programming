e13036016e6d401ac266ede7c06b95367a8c1652
Larry Madeo <lmadeo76@gmail.com>
2017-12-05 03:29:50 +0000

    first commit

---
+++ 0x1D-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches through array and finds index of match for value
+ * @array: pointer to an array of ints
+ * @size: size of array
+ * @value: value to search for in array
+ * Return: index of matching array element, or -1 if no match is found
+ **/
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i, ret = -1;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] == value)
+			ret = i;
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+	}
+	return (ret);
+}
