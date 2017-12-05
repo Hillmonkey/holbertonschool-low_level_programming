d0b54146cd51e48d0302687539f954cbcd2285f7
Larry Madeo <lmadeo76@gmail.com>
2017-12-05 07:57:18 +0000

    more functional

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -1,8 +1,9 @@
 #include "search_algos.h"
 
 /**
- *
- *
+ * print_array - print array of ints
+ * @array: array of ints
+ * @size: size of array
  **/
 void print_array(int *array, size_t size)
 {
@@ -21,12 +22,14 @@ void print_array(int *array, size_t size)
 			printf(" ");
 		first_pass = FALSE;
 	}
-	return;
 }
+
 /**
  * binary_search - search recursively in sorted list
- *
- *
+ * @array: array of ints
+ * @size: size of array
+ * @value: value to match in search
+ * Return: index of matching array element, or -1 if no match
  **/
 
 int binary_search(int *array, size_t size, int value)
@@ -35,14 +38,29 @@ int binary_search(int *array, size_t size, int value)
 
 	print_array(array, size);
 	mid = size / 2;
-	if (size != 0)
+
+	if (size < 1)
+		return (-1);
+	else if (size == 1)
+	{
+		if (array[0] == value)
+			return (0);
+		else
+			return (-1);
+	}
+	else  /* size > 1 */
+	{
 		if (array[mid] == value)
+		{
+			printf("====A=====\n");
 			return (mid);
+		}
 		if (array[mid] > value)
 			binary_search(array, mid, value);
-		else
-			binary_search(&array[mid], size - mid, value);
-	if (size == 0)
-		return (-1);
-	return (-1);
+		if (array[mid] < value)
+		{
+			print_array(&(array[mid + 1]), size - mid - 1);
+			binary_search(&(array[mid + 1]), size - mid - 1, value);
+		}
+	}
 }
