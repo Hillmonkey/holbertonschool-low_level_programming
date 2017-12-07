06bc68a6de3594d5b840bedb5c4781950363b0a4
Larry Madeo <lmadeo76@gmail.com>
2017-12-07 15:24:14 -0800

    first commit, jump search, mod header

---
+++ 0x1D-search_algorithms/100-jump.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - search in sorted array for value match
+ * @array: sorted array of ints
+ * @size: size of array
+ * @value: search for this value in array
+ * Return: index of matching element or -1 if no match
+ **/
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, hi, lo = 0, idx = 1;
+	double step = sqrt((double)size);
+
+	if (array == NULL || size == 0 || value < array[0])
+		return (-1);
+	hi = (size_t)step;
+
+	while (lo < size)
+	{
+		++idx;
+		if (!(array[lo] <= value && value <= array[hi]))
+			printf("Value checked array[%d] = [%d]\n", (int)lo, array[lo]);
+		else
+		{
+			printf("Value found between indexes [%d] and [%d]\n", (int)lo, (int)hi);
+			for (i = 0; i + lo <= hi && i + lo < size; i++)
+			{
+				printf("Value checked array[%d] = [%d]\n", (int)(i + lo), array[i + lo]);
+				if (array[i + lo] == value)
+					return (i + lo);
+			}
+		}
+		lo = hi;
+		hi  = (size_t)((double)idx * step);
+	}
+
+	return (-1);
+}
--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -12,5 +12,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int recurs_search(int *array, size_t lo, size_t hi, int value);
 void print_subarray(int *array, size_t lo, size_t hi);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS */
